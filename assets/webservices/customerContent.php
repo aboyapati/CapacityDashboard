11566943cc4b8d2b949dd3a94911742897828b73
Itekk 3009 <itekk.3009@gmail.com>
2018-04-22 13:33:22 -0400

    webservice change

--- assets/webservices/customerContent.php
+++ assets/webservices/customerContent.php
@@ -1,6 +1,12 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-    $data = '{"content":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys."}';
+$sub = $_POST['subComponentName'];
+
+if($sub == 'VRF') {
+	$data = '["VRF contents Line 1 Data","VRF contents Line 2 Data","VRF contents Line 3 Data","VRF contents Line 4 Data"]';
+} else {
+	$data = '["VRF contents Line 5 Data","VRF contents Line 6 Data","VRF contents Line 7 Data","VRF contents Line 8 Data"]';
+}
 
 echo $data;
 die();
